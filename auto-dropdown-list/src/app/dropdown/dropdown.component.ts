32f4de4125af1a65fb41ca1fad6e8727ddb5e623
Viswa <36505518+Viswa5558@users.noreply.github.com>
2018-02-20 17:19:15 +0530

    Update dropdown.component.ts

--- auto-dropdown-list/src/app/dropdown/dropdown.component.ts
+++ auto-dropdown-list/src/app/dropdown/dropdown.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import {FilterPipe } from '../filter.pipe';
+import { FilterPipe } from '../pipes/filter.pipe';
 
 @Component({
   selector: 'app-dropdown',
@@ -10,91 +10,10 @@ import {FilterPipe } from '../filter.pipe';
 
 export class DropdownComponent {
 
-  dropdownOptions = [];
+  
   selectedValue: string;
   expandOptions: boolean = false;
-
-  constructor() {
-    this.dropdownOptions = [
-      {
-        label: 'AZERBAIJAN',
-        value: '01'
-      },
-      {
-        label: 'ARMENIA',
-        value: '02'
-      },
-      {
-        label: 'ARABIA',
-        value: '03'
-      },
-
-      {
-        label: 'BAHRAIN',
-        value: '04'
-      },
-      {
-        label: 'BAHRAIN',
-        value: '05'
-      },
-      {
-        label: 'BANGLADESH',
-        value: '06'
-      },
-      {
-        label: 'CAMBODIA',
-        value: '07'
-      },
-      {
-        label: 'CHINA',
-        value: '08'
-      },
-      {
-        label: 'EAST',
-        value: '10'
-      },
-      {
-        label: 'INDIA',
-        value: '11'
-      },
-      {
-        label: 'INDONESIA',
-        value: '12'
-      },
-      {
-        label: 'IRAN',
-        value: '13'
-      },
-      {
-        label: 'IRAQ',
-        value: '14'
-      },
-      {
-        label: 'JAPAN',
-        value: '15'
-      },
-      {
-        label: 'JORDAN',
-        value: '16'
-      },
-      {
-        label: 'KAZAKHSTAN',
-        value: '17'
-      },
-      {
-        label: 'KUWAIT',
-        value: '18'
-      },
-      {
-        label: 'KYRGYZSTAN',
-        value: '19'
-      },
-      {
-        label: 'BAHRAIN',
-        value: '20'
-      }
-    ];
-  }
+  @Input('dropdownOptions') dropdownOptions;
 
     selectOption(option) {
       this.selectedValue = option.label;
